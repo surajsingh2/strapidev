67360b3cb7c2c5c2628b3dcc42f2260cf71bd458
Suraj <suraj@luxeveda.com>
2021-03-25 12:20:18 +0530

    initialized a strapi project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7349d0ac5a1150207df5b52957e0234a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0ee99ecc-ed99-4672-8da0-ead729c89280'
+};
